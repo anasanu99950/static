cc2e45daca9a4b5e1a09e86ddc47fba583005e2a
anasanu99950 <anasanu99950.com>
2026-01-14 16:32:33 +0530

    Ignore VS Code config files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+// import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('porfolio');
+  show='one'
+  name='mohammed anas'
+  place='thrissur'
+  qualification='BSc Computer Science'
+  job='MERN stack developer'
+  company='Inmakes'
+  userName='';
+  email='';
+  message='';
+
+
+  showTabOne(){
+    this.show='one'
+  }
+  showTabTwo(){
+    this.show='two'
+  }
+  showTabThree(){
+    this.show='three'
+  }
+
+
+  sendMessage(){
+   console.log("Username",this.userName);
+    console.log("Username",this.email);
+     console.log("Username",this.message);
+   
+  }
 }
